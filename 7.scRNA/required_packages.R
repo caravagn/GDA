47b8a4e7f22173f34e1730f708c8eb9a117fafd0
salvatore <militesalvatore@gmail.com>
2021-04-27 19:29:00 +0200

    scRNA-seq lesson, part 1

---
+++ 7.scRNA/required_packages.R
@@ -0,0 +1,8 @@
+install.packages("Seurat")
+install.packages("tidyverse")
+
+
+### install data ###
+
+download.file("https://www.dropbox.com/sh/pq4qzrywaja11l5/AAANcoCPPdBdrwy-c0xHH-dfa?dl=1", "./sc_data.zip")
+unzip("sc_data.zip")
